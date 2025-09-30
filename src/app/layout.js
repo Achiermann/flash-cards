ac74e572f9270b5339435e74ba696520beb6eaad
„Achiermann“ <andreasachermann@hotmail.de>
2025-09-30 12:58:54 +0200

    font and body-margin

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,29 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/main.css";
 import ClientWrapper from './clientWrapper';
 import toast, { Toaster } from 'react-hot-toast';
+import { Racing_Sans_One, Turret_Road, Funnel_Sans, Inter } from 'next/font/google';
 
+const racingSansOne = Racing_Sans_One({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-racing',
+});
+
+const turretRoad = Turret_Road({
+  subsets: ['latin'],
+  variable: '--font-turret',
+  weight: '400',
+});
+
+const funnelSans = Funnel_Sans({
+  subsets: ['latin'],
+  variable: '--font-funnel-sans',
+});
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-primary',
+});
 
 export const metadata = {
   title: { default: "Flash Cards", template: "%s | Flash Cards" },
@@ -15,7 +36,7 @@ export default function RootLayout({ children }) {
 
  return (
    <html lang="en">
-      <body>
+<body className={`${inter.variable} ${funnelSans.variable} ${racingSansOne.variable} ${turretRoad.variable}`}>
 <Toaster toastOptions={{className: 'toaster'}}/>
 <ClientWrapper>
   {children}
